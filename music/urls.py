c892239604490bbb1442d992257939ef69f9b355
ludwigwittgenstein2 <penpals.oranges14@gmail.com>
2016-09-06 18:23:13 -0400

    templates
    creating templates

--- music/urls.py
+++ music/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    # /music/71/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,8 +1,17 @@
 #Take a User Request and give them back something
 #99% User will request a webpage
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1> This is music app")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/music/' +str(album.id) + '/'
+        html += '<a href="' + url +  '">'+ album.album_title + '</a><br>'
 
-# Create your views here.
+    return HttpResponse(html)
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: "+ str(album_id) + "</h2>")
